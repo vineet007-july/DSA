161dce4ba4102bc15ad57aceeea85fbe0e440b25
Vineet Kumar <vkrmgs69@gmsil.com>
2022-10-26 21:18:58 +0530

    Another code reversing the linklist

--- Linked List/linkedListReverse.cpp
+++ Linked List/linkedListReverse.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+// Iterative C++ program to reverse a linked list
+#include <bits/stdc++.h>
 using namespace std;
 
+/* Link list node */
 struct Node {
 	int data;
 	struct Node* next;
@@ -15,22 +17,26 @@ struct LinkedList {
 	Node* head;
 	LinkedList() { head = NULL; }
 
+	/* Function to reverse the linked list */
 	void reverse()
 	{
+		// Initialize current, previous and next pointers
 		Node* current = head;
 		Node *prev = NULL, *next = NULL;
 
 		while (current != NULL) {
+			// Store next
 			next = current->next;
-
+			// Reverse current node's pointer
 			current->next = prev;
-
+			// Move pointers one position ahead.
 			prev = current;
 			current = next;
 		}
 		head = prev;
 	}
 
+	/* Function to print linked list */
 	void print()
 	{
 		struct Node* temp = head;
@@ -48,8 +54,10 @@ struct LinkedList {
 	}
 };
 
+/* Driver code*/
 int main()
 {
+	/* Start with the empty list */
 	LinkedList ll;
 	ll.push(20);
 	ll.push(4);
@@ -61,7 +69,7 @@ int main()
 
 	ll.reverse();
 
-	cout << "\nReversed Linked list \n";
+	cout << "\nReversed linked list \n";
 	ll.print();
 	return 0;
 }
